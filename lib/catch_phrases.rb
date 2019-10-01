4bc2ea0627e27c0b286105feca4c4d081e976b4d
Madhavi Thakur <madhavidadhwal91@gmail.com>
2019-10-01 17:21:57 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -17,10 +17,15 @@ def toadstool
 end
 
 def link 
+<<<<<<< HEAD
 puts "It's Dangerous To Go Alone! Take This."
 end 
 
 def any_phrase(phrase)
    phrase = "Do A Barrel Roll!"
   puts phrase
+=======
+puts ""
+
+>>>>>>> 662bf709d03aee1f7f52875f5e09f1d8af9ef232
 end 
